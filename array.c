7b2b248987c81c693d46b491f49ad04f3c5f0ea5
Ujwal <Walkerdoodle7@gmail.com>
2021-12-07 09:49:42 -0800

    Basic array program created

---
+++ array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void ins_beg(int n,int *a)
+{
+	int b,arr[20];
+	printf("enter the element to be inserted : ");
+	scanf("%d",&b);
+	for(int i=0;i<n;i++)
+		a[i]=a[i+1]
+	a[0]=b;
+}
+int main()
+{
+	int n,arr[20];
+	printf("Enter the number of elements : ");
+	scanf("%d",&n);
+	printf("Enter the array elements : ");
+	for(int i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	ins_beg(n,arr);
+	for(int i=0;i<=n;i++)
+	{
+		printf("%d",arr[i]
+	}
+	return 0;
+}
