2859e7cb3dcfc2298afe3d1b72d34ed3472e4821
Ujwal <Walkerdoodle7@gmail.com>
2021-12-08 08:47:50 -0800

    Challenge completed

--- array.c
+++ array.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int n,arr[20];
 int *p=&n;
-void ins_beg(int n,int *a)
+void ins_beg(int *n,int *a)
 {
 	int b;
 	printf("enter the element to be inserted : ");
 	scanf("%d",&b);
-	for(int i=n;i>=0;i--)
+	for(int i=*n;i>=0;i--)
 		a[i+1]=a[i];
 	a[0]=b;
+	*n=*n+1;
 }
 void ins_end(int *n,int *a)
 {
@@ -52,16 +53,47 @@ void del_pos(int *n,int *a)
 
 int main()
 {
-	
+	int choice;
 	printf("Enter the number of elements : ");
 	scanf("%d",&n);
 	printf("Enter the array elements : ");
 	for(int i=0;i<n;i++)
 		scanf("%d",&arr[i]);
-	del_pos(&n,arr);
+	do
+	{
+		printf("\n1.Insert at beginning\n2.Insert at end\n3.Insert at position\n4.Delete at beginning\n5.Delete at end\n6.Delete at position\n7.stop\nEnter your choice : ");
+		scanf("%d",&choice);
+		printf("\n%d\n",choice);
+		switch(choice)
+		{
+			case 1 : 
+				ins_beg(&n,arr);
+				break;
+			case 2 : 
+				ins_end(&n,arr);
+				break;
+			case 3 : 
+				ins_pos(&n,arr);
+				break;
+			case 4 : 
+				del_beg(&n,arr);
+				break;
+			case 5 : 
+				del_end(&n);
+				break;
+			case 6 : 
+				del_pos(&n,arr);
+				break;
+			case 7 : break;
+			default : 
+				printf("wrong input");
+				break;
+		}
 	for(int i=0;i<n;i++)
 	{
 		printf("%d ",arr[i]);
 	}
+	}while(choice != 7);
+	
 	return 0;
 }
